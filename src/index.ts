9a8c97239f681548fd2716835804aa9381b7cedf
chaza <mateo.chazarreta.et21.21@gmail.com>
2025-10-20 19:49:30 -0300

    server ya funcionando

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import app from "./server";
-app.listen(3000, () => {
-    console.log("Server disponible en el puerto " + 3000);
-})
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando correctamente 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor disponible en el puerto ${PORT}`);
+});
